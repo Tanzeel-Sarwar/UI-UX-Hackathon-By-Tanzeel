7ed6c9daaf58c00ba10d0b1096017e0773ad4cf8
Tanzeel Sarwar <tanzeelsarwar486@gmail.com>
2024-12-08 18:46:59 +0500

    Add files via upload

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+// interface HeaderProps {
+//   children: React.ReactNode
+// }
+
+// export function Header({ children }: HeaderProps) {
+//   return (
+//     <header>
+//       {children}
+//     </header>
+//   )
+// }
+
